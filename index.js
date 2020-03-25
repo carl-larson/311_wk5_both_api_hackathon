016d97ff0ce7f5bbcdf879b1613f7ba51b2181a8
carl-larson <clarson08@gmail.com>
2020-03-24 21:20:13 -0500

    still on Master, but I put in the basic express code for server operation

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('testing');
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 4001;
+
+app.get('/', (req, res) => {
+    res.send(`Welcome to our site!`);
+})
+
+app.listen(port, ()=> 
+    console.log(`Example app listening on port ${port}!`)
+)
