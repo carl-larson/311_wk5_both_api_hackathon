52077189ba4e4e5787e899ea5afa18318ea8409d
carl-larson <clarson08@gmail.com>
2020-03-26 20:06:32 -0500

    added dotenv to package.json and added more requirements to index

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require("body-parser");
+const employeesRouter = require('./routers/employees');
+require('dotenv').config();
+const app = express();
+
 
 const port = process.env.PORT || 4001;
+
+app.use(bodyParser.json())
+app.use('/', employeesRouter)
 
 app.get('/', (req, res) => {
     res.send(`Welcome to our site!`);
