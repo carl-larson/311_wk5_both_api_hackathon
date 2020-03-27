4f27f492c580741f5c7343c627ce5efc466ec181
carl-larson <clarson08@gmail.com>
2020-03-26 20:30:08 -0500

    added bodyParser line

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 4001;
 
 app.use(bodyParser.json())
 app.use("/employees", employeesRoutes);
->>>>>>> 7853546b1e83fcef6602f703d9cc98ebf39d8915
 
 app.get("/", (req, res) => {
     res.send(`Welcome to our site!`);
