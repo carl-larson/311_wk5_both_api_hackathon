ccfc9599d721365c076fb824a38a1825ab8174ac
csundbeck <csundbeck04@gmail.com>
2020-03-26 19:43:37 -0500

    Stood up database and integrated SQL in connections.js file

---
+++ sql/connections.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+class Connection {
+  constructor() {
+    if (!this.pool) {
+      console.log('creating connection...')
+      this.pool = mysql.createPool({
+        connectionLimit: 100,
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: 'admin'
+      })
+
+      return this.pool
+    }
+
+    return this.pool
+  }
+}
+
+const instance = new Connection()
+
+module.exports = instance;
